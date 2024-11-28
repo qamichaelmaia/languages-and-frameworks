30ed6d4e6fe87caac0bb3ec50ec0bea66e65064c
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-11-28 19:41:35 -0300

    fluxo de cadastro playwright

--- ebac-selenium-wdio/test/specs/lojaEbac.e2e.js
+++
@@ -1,8 +0,0 @@
-const homePage = require ("../pageobjects/home.page");
-
-describe('Loja EBAC', () => {
-    it('should access home page', async () => {
-        await homePage.open(); // Abre a página inicial
-        expect(await homePage.title).toBe("EBAC – Shop – Página de teste"); // Verifica se o título está correto
-    });
-});
